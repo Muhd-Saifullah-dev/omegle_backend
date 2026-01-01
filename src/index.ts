2bfa86a34be7a2e473d2587716778b682d588d34
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2026-01-01 10:04:17 +0500

    completed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express"
 import type { Application } from "express"
 import { PORT } from "./config/env.config";
+import authRoute from "./routes/auth.route";
+import userRoute from "./routes/user.route";
+import matchRoute from "./routes/match.route";
 
 const app:Application=express()
 
@@ -8,6 +11,11 @@ const app:Application=express()
 app.use(express.json({
     limit:'100mb'
 }))
+
+
+app.use("/api/auth",authRoute)
+app.use("/api/user",userRoute)
+app.use("/api/match",matchRoute)
 
 
 
